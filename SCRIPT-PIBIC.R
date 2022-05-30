0b09cf3ea6718927a1e593453ff5827ef27f969e
Ailton Matheus Avelino de Oliveira <87403622+ailtonm4th3us@users.noreply.github.com>
2022-05-30 11:59:12 -0300

    Armazenar

---
+++ SCRIPT-PIBIC.R
@@ -0,0 +1,25 @@
+library(devtools)  
+library(ENMTML)  
+
+
+ENMTML(pred_dir ="C:/Ailton/PIBIC/Biovar/Neotropic",
+       occ_file= "C:/Ailton/Occ/occurrence_all.txt",
+       result_dir="C:/Ailton/result_dir_all", # Cria o destinario para os resultados
+       proj_dir= "C:/Ailton/Biovar_futuro", # Diretorio contendo as var. do futuro
+       sp="sp", x="x", y="y", # Colunas nos arquivos de pontos de ocorrencia
+       min_occ=1,
+       colin_var=c(method="VIF"),  # Method to reduce variable collinearity
+       imp_var = TRUE,
+       eval_occ = NULL,
+       sp_accessible_area = NULL, # area acessiveis. Basicamente uma restritricao da spp (biomas, etc)
+       pres_abs_ratio = 1, # presenca - ausencia
+       pseudoabs_method = c(method='ENV_CONST'),
+       part=c(method='BOOT', replicates='10', proportion='0.7'),
+       save_part=FALSE,save_final=TRUE,
+       algorithm = c("MXS", "RDF", "SVM"),
+       cores=4,
+       thr=c(type='Jaccard'), # Limiar usado para previsoes de presenca-ausencia.
+       msdm=NULL, # Restricoes espaciais dos modelos  - Tem a priori e a posteriori
+       ensemble=c(method='SUP', metric='JACCARD'),
+       extrapolation=TRUE
+       )
